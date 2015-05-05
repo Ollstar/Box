bd51ac31ccf55cfc32913eee65de11df15935d77
Oliver Andrews <oltandrews@gmail.com>
2015-05-05 16:56:14 -0700

    completed project

--- 3-D Box/main.c
+++ 3-D Box/main.c
@@ -8,8 +8,43 @@
 
 #include <stdio.h>
 
+
+typedef struct {
+    float length;
+    float height;
+    float width;
+} Box;
+
+// functions prototypes
+float firstBoxFitsIntoSecondBox(Box, Box);
+float volumeOfBox(Box);
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+    Box firstBox = {1,2,5};
+    volumeOfBox(firstBox);
+    
+    Box secondBox = {1,4,5};
+    volumeOfBox(secondBox);
+    
+    firstBoxFitsIntoSecondBox(firstBox, secondBox);
+    
     return 0;
+    
+}
+
+
+
+// function definitions
+float firstBoxFitsIntoSecondBox(Box firstBox, Box secondBox) {
+    float fitsInto = volumeOfBox(firstBox)/volumeOfBox(secondBox);
+    printf("%f\n", fitsInto);
+    return fitsInto;
+}
+
+float volumeOfBox(Box boxOne) {
+    float theBox = boxOne.length*boxOne.height*boxOne.width;
+    printf("%f\n", theBox);
+    return theBox;
 }
